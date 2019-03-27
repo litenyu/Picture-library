bc630dbd1c178fe95965241bbda1ad3000e53551
litenyu <48281316+litenyu@users.noreply.github.com>
2019-03-27 19:28:24 +0800

    V1.0
    V1.0

---
+++ files/需求分析/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="需求分析.html",d="generationDate",e=new Date(1553686087304.09),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="2b9029c016d24c23be19a319cb2fedca",q="type",r="Axure:Page",s="name",t="需求分析",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="b2971a0cd61f4e62a515aaf783b874dd",Y="label",Z="friendlyType",ba="Image",bb="imageBox",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=1041,bi="height",bj=1943,bk="75a91ee5b9d042cfa01b8d565fe289c0",bl="imageOverrides",bm="images",bn="normal~",bo="images/需求分析/u225.png",bp="masters",bq="objectPaths",br="b2971a0cd61f4e62a515aaf783b874dd",bs="scriptId",bt="u225";
+return _creator();
+})());
